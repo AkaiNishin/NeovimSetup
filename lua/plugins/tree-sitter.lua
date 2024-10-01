8eb9c3143f5cbe3b3e1759f4b18ad0f20f9cb7a7
AkaiNishin <coreajustin288@gmail.com>
2024-09-30 23:11:53 -0600

    ADD:barbar

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,4 +1,5 @@
 return {
+    {
     "nvim-treesitter/nvim-treesitter",
     build = function()
         -- Use :TSUpdate to manually update parsers if needed
@@ -20,5 +21,28 @@ return {
             refactor = { enable = true },
         })
     end,
+    },
+    {
+        "romgrk/barbar.nvim",
+        config = function()
+            require('lazy').setup {
+                {'romgrk/barbar.nvim',
+                dependencies = {
+                    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+                    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+                },
+                init = function() vim.g.barbar_auto_setup = false end,
+                opts = {
+                    -- lazy.nvim will automatically call setup for you. 
+                    --put your options here, anything missing will use the default:
+                    -- animation = true,
+                    -- insert_at_start = true,
+                        -- …etc.
+                },
+                version = '^1.0.0', -- optional: only update when a new 1.x version is released
+                },
+            }
+        end,
+    }
 }
 
